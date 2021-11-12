afba596296cc27b813060ad8567ece7bf7e049e3
Jayly <65847850+JaylyDev@users.noreply.github.com>
2021-11-12 18:25:38 +0000

    Create PlayerDimTest.js

---
+++ BP/scripts/gametest/PlayerDimTest.js
@@ -0,0 +1,13 @@
+import { Commands, World } from 'mojang-minecraft';
+
+let count = 0;
+const players = World.getPlayers();
+var playercount = players.length;
+
+try {
+  let cmd = Commands.run('testfor @a[rm=0.01]', World.getDimension('nether'));
+  count = cmd.victim.length;
+
+if ((count / playercount) > 0.5) {
+  Commands.run('event entity @e[type=entity:terminator] terminator:find_portal') }
+} catch {}
