dc090cc3eab26dde3918d5b84641287a8c5d0272
bigguy69420 <91819282+bigguy69420@users.noreply.github.com>
2021-11-18 09:53:53 +0000

    Update PlayerDimTest.js

--- BP/scripts/gametest/PlayerDimTest.js
+++ BP/scripts/gametest/PlayerDimTest.js
@@ -1,13 +1,15 @@
 import { Commands, World } from 'mojang-minecraft';
 
-let count = 0;
-const players = World.getPlayers();
-var playercount = players.length;
+World.events.beforeChat.subscribe(eventChat => {
+  let count = 0;
+  const players = World.getPlayers();
+  var playercount = players.length;
 
-try {
-  let cmd = Commands.run('testfor @a[rm=0.01]', World.getDimension('nether'));
-  count = cmd.victim.length;
+  try {
+    let cmd = Commands.run('testfor @a[rm=0.01]', World.getDimension('nether'));
+    count = cmd.victim.length;
 
-if ((count / playercount) > 0.5) {
-  Commands.run('event entity @e[type=entity:terminator] terminator:find_portal') }
-} catch {}
+  if ((count / playercount) > 0.5) {
+    Commands.run('event entity @e[type=entity:terminator] terminator:find_portal') }
+  } catch {}
+}
