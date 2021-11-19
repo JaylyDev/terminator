3dc2a5db046e08cf6b658b5e170b14b0f84deaf7
Jayly <65847850+JaylyDev@users.noreply.github.com>
2021-11-19 23:11:17 +0000

    Update PlayerDimTest.js

--- BP/scripts/gametests/mojang-minecraft/PlayerDimTest.js
+++ BP/scripts/gametests/mojang-minecraft/PlayerDimTest.js
@@ -1,6 +1,6 @@
 import { Commands, World } from 'mojang-minecraft';
 
-World.events.beforeChat.subscribe(eventChat => {
+World.events.tick.subscribe(() => {
   let playercountresult = 0;
   const players = World.getPlayers();
   var playercount = players.length;
@@ -8,30 +8,30 @@ World.events.beforeChat.subscribe(eventChat => {
   try {
     let playercmd = Commands.run('testfor @a[rm=0.01]', World.getDimension('overworld'));
     let terminatorcmd = Commands.run('testfor @e[rm=0.01,type=entity:terminator]', World.getDimension('overworld'));
-    playercountresult = cmd.victim.length;
+    playercountresult = playercmd.victim.length;
 
-    if ((playercountresult / playercount) > 0.5 && terminatorcmd == 0) {
+    if ((playercountresult / playercount) > 0.5 && terminatorcmd.victim.length == 0) {
       Commands.run('event entity @e[type=entity:terminator] terminator:find_portal_to_overworld') 
     }
   } catch {
     try {
       let playercmd = Commands.run('testfor @a[rm=0.01]', World.getDimension('nether'));
       let terminatorcmd = Commands.run('testfor @e[rm=0.01,type=entity:terminator]', World.getDimension('nether'));
-      playercountresult = cmd.victim.length;
+      playercountresult = playercmd.victim.length;
 
-      if ((playercountresult / playercount) > 0.5 && terminatorcmd == 0) {
+      if ((playercountresult / playercount) > 0.5 && terminatorcmd.victim.length == 0) {
         Commands.run('event entity @e[type=entity:terminator] terminator:find_portal_to_nether') 
       }
     } catch {
       try {
         let playercmd = Commands.run('testfor @a[rm=0.01]', World.getDimension('the end'));
         let terminatorcmd = Commands.run('testfor @e[rm=0.01,type=entity:terminator]', World.getDimension('the end'));
-        playercountresult = cmd.victim.length;
+        playercountresult = playercmd.victim.length;
 
-        if ((playercountresult / playercount) > 0.5 && terminatorcmd == 0) {
+        if ((playercountresult / playercount) > 0.5 && terminatorcmd.victim.length == 0) {
           Commands.run('event entity @e[type=entity:terminator] terminator:find_portal_to_the_end') 
         }
       } catch {}
     }
   }
-}
+})
