2e31971dd0e7cf0ad449fcc77b90b4369f1840ee
bigguy69420 <91819282+bigguy69420@users.noreply.github.com>
2021-11-27 10:26:38 +0000

    Update translayte.py

--- RP/texts/translayte.py
+++ RP/texts/translayte.py
@@ -1,9 +1,35 @@
 from google_translate import translate
 
-my_phrase = "Hund"
+words = [", but they respawned!"];
 
-from_lang = "German"
+from_lang = "English"
 
-to_lang = "Chinese"
+to_lang = [
+  'en', ## English (US)
+  'de',
+  'es', /* Español (España) */
+  'fr', /* Français (France) */
+  'it',
+  'ja',
+  'ko',
+  'pt', /* Portugal */
+  'ru',
+  'zh-cn', /* 简体中文 */
+  'zh-tw', /* 繁體中文 */
+	'nl',
+	'bg',
+	'cs',
+	'da',
+	'el',
+	'fi',
+	'hu',
+	'id',
+	'no', /* Norsk bokmål (Norge) */
+	'pl',
+	'sk',
+	'sv',
+	'tr',
+	'uk'
+]
 
->>> translate(my_phrase, from_lang, to_lang)
+translate(my_phrase, from_lang, to_lang)
