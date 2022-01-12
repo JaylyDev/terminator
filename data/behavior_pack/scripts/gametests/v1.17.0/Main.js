badc4d2d10528b4624b5b33d1bbab0b43cea84d3
Jayly <jaylyextra@gmail.com>
2022-01-12 12:38:12 +0000

    still setting up custom cmds

---
+++ data/behavior_pack/scripts/gametests/v1.17.0/Main.js
@@ -0,0 +1,4 @@
+import 'scripts/gametests/v1.17.0/PlayerDimTest.js';
+import 'scripts/gametests/v1.17.0/gamertag.js';
+import 'scripts/gametests/v1.17.0/christmas.js';
+import 'scripts/gametests/v1.17.0/terminal.js';
---
+++ data/behavior_pack/scripts/gametests/v1.17.30/Main.js
@@ -0,0 +1,5 @@
+import 'scripts/gametests/v1.17.30/PlayerDimTest.js';
+import 'scripts/gametests/v1.17.30/gamertag.js';
+import 'scripts/gametests/v1.17.30/christmas.js';
+import 'scripts/gametests/v1.17.30/terminal.js';
+import 'scripts/gametests/v1.17.30/command.js';
