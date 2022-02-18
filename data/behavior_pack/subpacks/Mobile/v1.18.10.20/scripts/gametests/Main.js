daa6f5b8896db8adf011af2d76c146da816c02e6
Jayly <jaylyextra@gmail.com>
2022-02-18 17:53:33 +0000

    62th revisiion
    - remove some javascript files
    - fix changelog text
    - fixed #40

--- data/behavior_pack/subpacks/Mobile/v1.18.10.20/scripts/gametests/Main.js
+++ data/behavior_pack/subpacks/Mobile/v1.18.10.20/scripts/gametests/Main.js
@@ -1,5 +1,5 @@
 import 'scripts/gametests/PlayerDimTest.js';
 import 'scripts/gametests/gamertag.js';
-import 'scripts/gametests/terminal.js';
 import 'scripts/gametests/commands/index.js';
 import 'scripts/gametests/commands/teleport.js';
+import 'scripts/minecraft/command.js';
