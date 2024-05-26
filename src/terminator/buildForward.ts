b4ee57ad0312d17842af0133009666421f19b473
Jayly <65847850+JaylyDev@users.noreply.github.com>
2024-05-26 08:57:15 +0100

    i blame mojang dodgy code

--- src/terminator/buildForward.ts
+++ src/terminator/buildForward.ts
@@ -4,11 +4,27 @@ import {
   VECTOR3_EAST,
   VECTOR3_NORTH,
   VECTOR3_SOUTH,
+  VECTOR3_UP,
   VECTOR3_WEST,
   Vector3Builder,
 } from "@minecraft/math";
 import { MinecraftBlockTypes } from "@minecraft/vanilla-data";
 import { TerminatorEntity } from "./terminator";
+
+// Vector3 values for Math
+//
+// VECTOR3_NORTH = (0, 0, 1)
+// VECTOR3_EAST = (1, 0, 0)
+// VECTOR3_SOUTH = (0, 0, -1)
+// VECTOR3_WEST = (-1, 0, 0)
+
+// Vector3 values for Minecraft game
+// Yes, East and West are flipped, I blame the Mojang's dodgy code.
+//
+// North (0 degree) = (0, 0, 1)
+// East (90 degree) = (-1, 0, 0)
+// South (-180 degree) = (0, 0, -1)
+// West (-90 degree) = (1, 0, 0)
 
 class BridgePosition {
   private constructor() {
@@ -19,16 +35,16 @@ class BridgePosition {
     return new Vector3Builder(VECTOR3_NORTH).add(VECTOR3_DOWN);
   }
   public static get West() {
-    return new Vector3Builder(VECTOR3_WEST).add(VECTOR3_DOWN);
+    return new Vector3Builder(VECTOR3_EAST).add(VECTOR3_DOWN);
   }
   public static get East() {
-    return new Vector3Builder(VECTOR3_EAST).add(VECTOR3_DOWN);
+    return new Vector3Builder(VECTOR3_WEST).add(VECTOR3_DOWN);
   }
   public static get South() {
     return new Vector3Builder(VECTOR3_SOUTH).add(VECTOR3_DOWN);
   }
   public static get NorthEast() {
-    return [this.North, this.West];
+    return [this.North, this.East];
   }
   public static get SouthWest() {
     return [this.South, this.West];
@@ -38,6 +54,49 @@ class BridgePosition {
   }
   public static get NorthWest() {
     return [this.North, this.West];
+  }
+}
+
+class MiningPosition {
+  private constructor() {
+    throw new TypeError("Illegal Constructor.");
+  }
+
+  public static get NorthUp() {
+    return new Vector3Builder(VECTOR3_NORTH).add(VECTOR3_UP);
+  }
+  public static get WestUp() {
+    return new Vector3Builder(VECTOR3_EAST).add(VECTOR3_UP);
+  }
+  public static get EastUp() {
+    return new Vector3Builder(VECTOR3_WEST).add(VECTOR3_UP);
+  }
+  public static get SouthUp() {
+    return new Vector3Builder(VECTOR3_SOUTH).add(VECTOR3_UP);
+  }
+  public static get North() {
+    return [new Vector3Builder(VECTOR3_NORTH), this.NorthUp];
+  }
+  public static get West() {
+    return [new Vector3Builder(VECTOR3_EAST), this.WestUp];
+  }
+  public static get East() {
+    return [new Vector3Builder(VECTOR3_WEST), this.EastUp];
+  }
+  public static get South() {
+    return [new Vector3Builder(VECTOR3_SOUTH), this.SouthUp];
+  }
+  public static get NorthEast() {
+    return [...this.North, ...this.East];
+  }
+  public static get SouthWest() {
+    return [...this.South, ...this.West];
+  }
+  public static get SouthEast() {
+    return [...this.South, ...this.East];
+  }
+  public static get NorthWest() {
+    return [...this.North, ...this.West];
   }
 }
 
@@ -65,38 +124,85 @@ system.afterEvents.scriptEventReceive.subscribe(
       return;
 
     const terminator = new TerminatorEntity(event.sourceEntity);
-    const location = terminator.location;
     switch (event.id) {
       case TerminatorBuildDirection.North:
-        terminator.placeBlock(location.add(BridgePosition.North), bridgeBlock);
+        for (const direction of MiningPosition.North) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
+        terminator.placeBlock(
+          terminator.location.add(BridgePosition.North),
+          bridgeBlock
+        );
         break;
       case TerminatorBuildDirection.West:
-        terminator.placeBlock(location.add(BridgePosition.West), bridgeBlock);
+        for (const direction of MiningPosition.West) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
+        terminator.placeBlock(
+          terminator.location.add(BridgePosition.West),
+          bridgeBlock
+        );
         break;
       case TerminatorBuildDirection.East:
-        terminator.placeBlock(location.add(BridgePosition.East), bridgeBlock);
+        for (const direction of MiningPosition.East) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
+        terminator.placeBlock(
+          terminator.location.add(BridgePosition.East),
+          bridgeBlock
+        );
         break;
       case TerminatorBuildDirection.South:
-        terminator.placeBlock(location.add(BridgePosition.South), bridgeBlock);
+        for (const direction of MiningPosition.South) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
+        terminator.placeBlock(
+          terminator.location.add(BridgePosition.South),
+          bridgeBlock
+        );
         break;
       case TerminatorBuildDirection.NorthEast:
+        for (const direction of MiningPosition.NorthEast) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
         for (const direction of BridgePosition.NorthEast) {
-          terminator.placeBlock(location.add(direction), bridgeBlock);
+          terminator.placeBlock(
+            terminator.location.add(direction),
+            bridgeBlock
+          );
         }
         break;
       case TerminatorBuildDirection.SouthWest:
+        for (const direction of MiningPosition.SouthWest) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
         for (const direction of BridgePosition.SouthWest) {
-          terminator.placeBlock(location.add(direction), bridgeBlock);
+          terminator.placeBlock(
+            terminator.location.add(direction),
+            bridgeBlock
+          );
         }
         break;
       case TerminatorBuildDirection.SouthEast:
+        for (const direction of MiningPosition.SouthEast) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
         for (const direction of BridgePosition.SouthEast) {
-          terminator.placeBlock(location.add(direction), bridgeBlock);
+          terminator.placeBlock(
+            terminator.location.add(direction),
+            bridgeBlock
+          );
         }
         break;
       case TerminatorBuildDirection.NorthWest:
+        for (const direction of MiningPosition.NorthWest) {
+          terminator.breakBlock(terminator.location.add(direction));
+        }
         for (const direction of BridgePosition.NorthWest) {
-          terminator.placeBlock(location.add(direction), bridgeBlock);
+          terminator.placeBlock(
+            terminator.location.add(direction),
+            bridgeBlock
+          );
         }
         break;
 
